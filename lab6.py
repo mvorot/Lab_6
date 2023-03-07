76b776ca94e0ed5bc66fc91d0a287d10f0eaf522
mvorot <mikevorot.com>
2023-03-07 09:34:18 -0500

    Wrote an encoder function and the menu

---
+++ lab6.py
@@ -0,0 +1,36 @@
+
+def encoder(password):
+    encoded_password = ''
+    for i in password:
+        encoded_password += str(int(i) + 3)
+    return encoded_password
+
+def print_menu():
+    print('''Menu
+-------------
+1. Encode
+2. Decode
+3. Quit\n''')
+
+while True:
+    print_menu()
+    option = int(input('Please enter an option: '))
+
+    if option == 1:
+        password = input('Please enter your password to encode: ')
+        encoder(password)
+        print('Your password has been encoded and stored!\n')
+    elif option == 2:
+        pass
+    elif option == 3:
+        pass
+    else:
+        pass
+
+
+
+
+
+
+
+
