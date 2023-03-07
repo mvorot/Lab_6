c760c524570aac5576f7b1ac31e4b7d33c360bb9
willo <wmandi.mcginley@ufl.edu>
2023-03-07 10:04:33 -0500

    Updated the file with a decoder

--- lab6.py
+++ lab6.py
@@ -6,6 +6,12 @@ def encoder(password):
         encoded_password += str(int(i) + 3)
     return encoded_password
 
+def decoder(encoded_password):
+    decoded_password = ''
+    for i in encoded_password:
+        decoded_password += str(int(i) - 3)
+    return decoded_password
+
 def print_menu():
     print('''Menu
 -------------
@@ -19,12 +25,13 @@ while True:
 
     if option == 1:
         password = input('Please enter your password to encode: ')
-        encoder(password)
+        encoded_password = encoder(password)
         print('Your password has been encoded and stored!\n')
     elif option == 2:
-        pass
+        decoded_password = decoder(encoded_password)
+        print(f'The encoded password is {encoded_password}, and the original password is {decoded_password}.')
     elif option == 3:
-        pass
+        exit()
     else:
         pass
 
